d7f9dc187788dd4f7fad0b96ab3c8025714dfcef
hadrian <iangreen2.00@gmail.com>
2021-01-22 11:32:48 -0800

    Added Ian's initial work

---
+++ classwork/ian_evenOddTable.c
@@ -0,0 +1,17 @@
+#define LOWER_LIMIT -10
+#define UPPER_LIMIT 10
+
+int square(int);
+
+int main(int argc, char *argv[]){
+    
+    int lower = LOWER_LIMIT;
+    int upper = UPPER_LIMIT;
+
+    
+
+}
+
+inline int square(int b){
+    return b * b
+}
