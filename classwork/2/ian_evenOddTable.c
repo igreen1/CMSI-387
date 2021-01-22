004f21930f7b5d58e104d79b5ede5280596e1e60
hadrian <iangreen2.00@gmail.com>
2021-01-22 11:46:25 -0800

    Ian forgot to add odd/even count; added two counters

--- classwork/2/ian_evenOddTable.c
+++ classwork/2/ian_evenOddTable.c
@@ -11,16 +11,22 @@ int main(int argc, char *argv[]){
     int lower = LOWER_LIMIT;
     int upper = UPPER_LIMIT;
     char parity[5];
+    int evenCount = 0;
+    int oddCount = 0;
 
     for(int i = lower; i <= upper; i++){
         if(i % 2 == 0){
             strcpy(parity, "EVEN");
+            evenCount++;
         } else {
             strcpy(parity, "ODD");
+            oddCount++;
 
         }
         printf("%5d\t%5d\t%4s\n",i, square(i), parity);
-    }   
+    }
+    printf("# odd numbers = %d\n", oddCount);
+    printf("# even numbers = %d\n", evenCount);
 }
 
 inline int square(int b){
