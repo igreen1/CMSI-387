f797e4b226ab88f0d4ec14959ec280f74e7d66bc
Ian <40191185+igreen1@users.noreply.github.com>
2021-01-26 12:29:01 -0800

    Quick updates based on classwork 02 feedback
    Removed some the test files, consolidated declarations, and moved #define after #imports following conventions outline by professor Johnson

--- classwork/classwork03/goFish.c
+++ classwork/classwork03/goFish.c
@@ -1,28 +1,24 @@
-#define INPUT_SIZE 25
-
 #include <stdio.h>
+
+#define INPUT_SIZE 25
 
 int main(int argc, char * argv[]){
 
-    int input[INPUT_SIZE];
+    int input[INPUT_SIZE],
+        entryNumber = 0;
     // not essential but nice
     for(int i = 0 ; i < INPUT_SIZE; i++)
         input[i]=0;
-    int entryNumber = 0;
     
     do{
         printf("\nEnter a number: ");
         scanf("%d", &input[entryNumber]);
     }while( input[entryNumber++] != -9999 && entryNumber < INPUT_SIZE);
 
-    // for(int i = 0; i < INPUT_SIZE; i++){
-    //     printf("%d: %d\n", i, input[i]);
-    // }
-
     if(entryNumber != INPUT_SIZE){
         input[--entryNumber] = 0; // Don't include the -9999
     }
-    entryNumber--; //Don't include the end input (now zero)
+    entryNumber--; //Don't include the value after the last value (since iteration occurs while checking)
 
     int sum = 0;
     for(int i = 0; i <= entryNumber; i++)
